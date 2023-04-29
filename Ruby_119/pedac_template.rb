71c713ba75bea29acedd9a68b32c33ca3160eef5
David Stephenson <davidstephenson8@gmail.com>
2023-04-28 20:16:26 -0600

    just some CHCHACHACHACHACAHC CHANGES

---
+++ Ruby_119/pedac_template.rb
@@ -0,0 +1,43 @@
+=begin
+==============
+Problem
+
+------------------------
+-  Inputs:
+-  Output:
+---
+**Explicit Requirements**
+1.
+2.
+3.
+---
+**Implicit Requirements** (if needed)
+1.
+2.
+3.
+---
+Examples 
+----------------------------------
+**Examples:**
+-  Example 1
+  -  Inputs:
+  -  Output:
+-  Example 2
+  -  Inputs:
+  -  Output:
+---
+Data Structures
+--------------
+
+---
+Scratch Pad/Working Area
+--------------------------------------------------
+
+
+
+
+--------------------------------------------------
+Algorithm
+---------
+
+=end
