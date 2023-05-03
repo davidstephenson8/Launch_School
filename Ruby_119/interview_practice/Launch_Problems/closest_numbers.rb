cb79c13b996f02f78681784304d9d342576432a4
David Stephenson <davidstephenson8@gmail.com>
2023-05-02 21:16:01 -0600

    some other stuff

--- Ruby_119/interview_practice/Launch_Problems/closest_numbers.rb
+++ Ruby_119/interview_practice/Launch_Problems/closest_numbers.rb
@@ -22,12 +22,12 @@ def generate_pairs(arr)
   pairs = []
   arr.each_with_index do |num1, index1|
     arr.each_with_index do |num2, index2|
-      unless index1 == index2
+      if index1 < index2
         pairs << [num1, num2]
       end
     end
   end
-  pairs.uniq
+  pairs
 end  
 
 def closest_numbers(arr)
@@ -44,8 +44,62 @@ def closest_numbers(arr)
   lowest_pair
 end
 
+p generate_pairs([5, 25, 15, 11, 20])
+p generate_pairs([19, 25, 32, 4, 27, 16])
+
 p closest_numbers([5, 25, 15, 11, 20]) == [15, 11]
 p closest_numbers([19, 25, 32, 4, 27, 16]) == [25, 27]
 p closest_numbers([12, 7, 17]) == [12, 7]
 
 # The tests above should print "true".
+
+
+#generate pairs from a string
+
+def pairs(string)
+  results = []
+  characters = string.chars
+  characters.each_with_index do |char1, index1|
+    characters.each_with_index do |char2, index2|
+      if index1 <= index2
+        results << characters[index1..index2].join 
+      end
+    end
+  end
+  results
+end
+
+p pairs("swagilecious")
+
+#take each group of 3 from the string. Don't return slices less than 3.
+
+def slices(string)
+  index = 0
+  results = []
+  loop do 
+    substring = string[index..index + 2]
+    results << substring if substring.size > 2 
+    index += 3
+    break if index >= string.size
+  end
+  results
+end
+
+p slices("eduardo ramirezxx")
+
+#actually do return the remaining slices
+
+def slices_with_remainder(string)
+  index = 0
+  results = []
+  loop do 
+    substring = string[index..index + 2] 
+    results << substring
+    index += 3
+    break if index >= string.size
+  end
+  results
+end
+
+p slices_with_remainder("eduardo ramirezxx")
+
--- Ruby_119/interview_practice/problem_helps.rb
+++ Ruby_119/interview_practice/problem_helps.rb
@@ -13,10 +13,30 @@ def substrings(string)
   substrings
 end
 
-p substrings("this is a string") == substrings("this is a string").uniq
+p substrings("this is a string") #== substrings("this is a string").uniq
 p substrings("supercalifragilisticexpialidocious") == substrings("supercalifragilisticexpialidocious")
 
-p substrings("this is a string").count("s")
-p substrings("supercalifragilisticexpialidocious") == substrings("supercalifragilisticexpialidocious")
+#create all pairs
+
+def pairs(array)
+  pairs = []
+  array.each_with_index do | ele1, index1 |
+    array.each_with_index do | ele2, index2 |
+      if index1 < index2
+        pairs << [ele1, ele2]
+      end
+    end
+  end
+  pairs
+end
+
+p pairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+p pairs([2, 4, 6, 8, 10])
+
+#create all possible permutations 
+
+
+
+#create all 
 
 
