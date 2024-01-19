f75f6fad51c636bf9037c3e4aba5599f73952c6c
David Stephenson <davidstephenson8@gmail.com>
2024-01-18 21:04:09 -0700

    more easy exercise work

---
+++ Ruby_130/Exercises/Easy2/4.rb
@@ -0,0 +1,14 @@
+def count(*arguments)
+  count = 0
+  arguments.each do |arg|
+    count += 1 if yield(arg)
+  end
+  count
+end
+
+p count(1, 3, 6) { |value| value.odd? } == 2
+p count(1, 3, 6) { |value| value.even? } == 1
+p count(1, 3, 6) { |value| value > 6 } == 0
+p count(1, 3, 6) { |value| true } == 3
+p count() { |value| true } == 0
+p count(1, 3, 6) { |value| value - 6 } == 3
